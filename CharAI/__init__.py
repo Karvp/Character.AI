b05b243be9f817c6cf842ac02f767a4718546891
Drevz <66773786+Karvp@users.noreply.github.com>
2024-02-16 18:42:29 +0700

    Update __init__.py

--- CharAI/__init__.py
+++ CharAI/__init__.py
@@ -1,15 +1,15 @@
 from CharAI.CAI import Client, User, Post, Character, Chat, Chat2, Comment, Owner, Topic, Feed, History, CAI
-from CharAI.CAI import Message, Persona, Category, Voice, MessagePage
-from CharAI.CAI import Post_type, Data, Subcription, Visibility, Posts_sort
-from CharAI.CAI import BASE_AVATAR_URL, BASE_IMAGE_PATH, BASE_TOPIC_AVATAR_URL, BASE_AVATAR_UPLOAD_PATH, BASE_POST_IMG_UPLOAD_PATH, DEFAULT_LAST_MSG_PAGE
+from CharAI.CAI import Message, Persona, Category, Voice, MessagePage, Messaging
+from CharAI.CAI import Data, Subcription, Visibility, PostSort
+from CharAI.CAI import AVATAR_URL, IMAGE_PATH, TOPIC_AVATAR_URL, AVATAR_UPLOAD_PATH, POST_IMG_UPLOAD_PATH, DEFAULT_LAST_MSG_PAGE
 
-from CharAI import new, helper
+from CharAI import new, helper, errors
 
 __all__ = [
     'Client', 'User', 'Post', 'Character', 'Chat', 'Chat2',
-    'Comment', 'Owner', 'Topic', 'Feed', 'History', 'CAI',
+    'Comment', 'Owner', 'Topic', 'Feed', 'History', 'CAI', 'errors',
     'new', 'Message', 'Persona', 'Category', 'Voice', 'helper', 'MessagePage',
-    'Post_type', 'Data', 'Subcription', 'Visibility', 'Posts_sort',
-    'BASE_AVATAR_URL', 'BASE_IMAGE_PATH', 'BASE_TOPIC_AVATAR_URL',
-    'BASE_AVATAR_UPLOAD_PATH', 'BASE_POST_IMG_UPLOAD_PATH', 'DEFAULT_LAST_MSG_PAGE'
+    'Data', 'Subcription', 'Visibility', 'PostSort', 'Messaging',
+    'AVATAR_URL', 'IMAGE_PATH', 'TOPIC_AVATAR_URL',
+    'AVATAR_UPLOAD_PATH', 'POST_IMG_UPLOAD_PATH', 'DEFAULT_LAST_MSG_PAGE'
 ]
